cf93e3f5dcd2669deb953ba1ac83db19e29a6a0f
Nicole Radford <nradford0171@gmail.com>
2015-03-03 17:44:14 -0700

    updated rules and neighbor count

--- conways_game_of_life.rb
+++ conways_game_of_life.rb
@@ -1,53 +1,88 @@
 #create Board
-class Board
-  def show_grid
-@grid = [
-  ['','','','0','0','0','','','',''],
-  ['','','','0','0','0','','','',''],
-  ['','','','0','0','0','','','',''],
-  ['','','','0','0','0','','','',''],
-  ['','','','0','0','0','','','',''],
-  ['','','','0','0','0','','','',''],
-  ['','','','0','0','0','','','',''],
-  ['','','','0','0','0','','','',''],
-  ['','','','0','0','0','','','','']
-]
-@grid.each do |row|
-puts row.map{ |col| col}.join(' ')
+
+    @grid = [
+      [' ',' ',' ','0','0','0',' ',' ',' ',' '],
+      [' ',' ','0 ',' ',' ','0',' ',' ',' ',' '],
+      [' ',' ',' ','0','0','0',' ',' ',' ',' '],
+      [' ',' ',' ',' ',' ','0',' ',' ',' ',' '],
+      [' ',' ',' ','0','0','0',' ',' ',' ',' '],
+      [' ',' ',' ','0',' ','0',' ',' ',' ',' '],
+      [' ',' ',' ','0','0','0',' ',' ',' ',' '],
+      [' ',' ',' ','0',' ','0',' ',' ',' ',' '],
+      [' ',' ',' ','0','0','0',' ',' ',' ',' '],
+      [' ',' ',' ','0','0','0',' ',' ',' ',' ']
+    ]
+    @grid2 = [
+      [' ',' ',' ','0','0','0',' ',' ',' ',' '],
+      [' ',' ',' ','0',' ','0',' ',' ',' ',' '],
+      [' ',' ',' ','0','0','0',' ',' ',' ',' '],
+      [' ',' ',' ','0',' ','0',' ',' ',' ',' '],
+      [' ',' ',' ','0','0','0',' ',' ',' ',' '],
+      [' ',' ',' ','0',' ','0',' ',' ',' ',' '],
+      [' ',' ',' ','0','0','0',' ',' ',' ',' '],
+      [' ',' ',' ','0',' ','0',' ',' ',' ',' '],
+      [' ',' ',' ','0','0','0',' ',' ',' ',' '],
+      [' ',' ',' ','0','0','0',' ',' ',' ',' ']
+    ]
+
+# @grid[0][0] = 'r'
+
+
+  def show_board
+    @grid.each do |row|
+      puts row.map{ |col| col}.join(' ')
+    end
+  end
+
+  def cell_alive
+    @alive ? [2,3].include?(alive_neighbors_count) : alive_neighbors_count == 3
+  end
+
+  def alive_neighbors_count(x, y)
+  
+    @alive_counter = 0
+     @alive_counter += 1 if @grid[x-1 < 0 ? 9 : x-1][y-1 < 0 ? 9 : y-1] == '0' 
+     @alive_counter += 1 if @grid[x-1 < 0 ? 9 : x-1][y] == '0' 
+     @alive_counter += 1 if @grid[x-1 < 0 ? 9: x-1][y+1 > 9 ? 0: y+1] == '0'
+     @alive_counter += 1 if @grid[x][y-1< 0 ? 9: y-1]   == '0'
+     @alive_counter += 1 if @grid[x][y+1 > 9 ? 0: y+1]   == '0'
+     @alive_counter += 1 if @grid[x+1 > 9 ? 0: x+1][y-1< 0 ? 9: y-1] == '0'
+     @alive_counter += 1 if @grid[x+1 > 9 ? 0: x+1][y]   == '0'
+    @alive_counter += 1 if @grid[x+1 > 9 ? 0: x+1][y+1 > 9 ? 0: y+1] == '0' 
+    return @alive_counter
+  end
+  def check_alive_count
+    @grid.each_with_index do |row,row_index|
+      row.each_with_index do |cell, col_index|
+        @row_index = row_index
+        @col_index = col_index
+         @grid2[row_index][col_index] = apply_rules(alive_neighbors_count(row_index, col_index))
+
+    end
+    end  
+  end
+  def replace_grid
+    @grid.inject {@grid2}
+    show_board
+  end
+
+
+
+def apply_rules(numberofaliveneighbors)
+  if numberofaliveneighbors <= 1 
+    return ' '
+  elsif numberofaliveneighbors >= 4
+    return ' ' 
+  elsif numberofaliveneighbors == 3
+    return '0'
+  elsif numberofaliveneighbors == 2
+    return @grid[@row_index][@col_index]
+  end
 end
-end
-#def alive_or_dead
- # @grid.each do |row, col|
-  #@alive = if @grid[row][col]=0
-#end
-end
-def alive_neighbors_count(x,y)
-  @alive_counter = 0
-  @alive_counter += 1 if @board[x-1][y-1] == 0
-  @alive_counter += 1 if @board[x-1][y]   == 0
-  @alive_counter += 1 if @board[x-1][y+1] == 0
-  @alive_counter += 1 if @board[x][y-1]   == 0
-  @alive_counter += 1 if @board[x][y+1]   == 0
-  @alive_counter += 1 if @board[x+1][y-1] == 0
-  @alive_counter += 1 if @board[x+1][y]   == 0
-  @alive_counter += 1 if @board[x+1][y+1] == 0 
-  return @alive_counter
-end
-def check_alive
-  @grid.each
-  alive_neighbors_count(x,y)
-end
-#@neigbors=[[-1, -1],[-1,0], [-1, 1],[0,-1],[0,1],[1,-1],[1,0],[1,1]]
+# initalize
+check_alive_count
+replace_grid
+# alive_neighbors_count(0,0)
 
 
 
-
-
-end
-
-#checking neigbors
-
-
-
-Board
-#if 0 true,
